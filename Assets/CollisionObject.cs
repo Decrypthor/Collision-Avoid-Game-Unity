64f08ca497167eeaf2ca64ddd8edadd626bebe89
decrypthor <rkathire@usc.edu>
2019-09-13 03:12:19 -0700

    first commit

---
+++ Assets/CollisionObject.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class CollisionObject : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    private void OnCollisionEnter(Collision collisioninfo)
+    {
+        
+        if (collisioninfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
